043e1419731dc5bf5f1cd0c8dd13e0f787bd1045
NAVIN0507 <navinofficial2005@gmail.com>
2025-05-27 11:09:45 +0530

    Added routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
-      <h1 className='text-2xl '>Welcome !</h1>
+      <h1 className='text-2xl underline'>Welcome !</h1>
+      <Button>Click Me</Button>
     </div>
   )
 }
