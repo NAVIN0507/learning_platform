cdbf3a8d4aaa316e3b44c2394e5217055ee26f0e
NAVIN0507 <navinofficial2005@gmail.com>
2025-05-27 14:17:45 +0530

    Added NavBAr

---
+++ components/Navitems.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+const navItems = [
+    {label:'Home' , href:'/'},
+    {label:'Companions' , href:'/companions'},
+    {label:'My Journy' , href:'my-journy'}
+]
+const Navitems = () => {
+    const pathName = usePathname();
+  return (
+  <nav className='flex items-center gap-4'>
+  {navItems.map(({label , href})=>{
+    const isActive = pathName === href
+    return(
+    <Link href={href} key={label}> <span className={`${isActive ?'underline':''}`}>{label}</span></Link>
+)})}
+  </nav>
+  )
+}
+
+export default Navitems
