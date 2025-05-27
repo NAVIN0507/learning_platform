bc2ad9a56ab05ecbab3cabcbaf397d3d2834a088
NAVIN0507 <navinofficial2005@gmail.com>
2025-05-27 16:20:55 +0530

    Added select

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -15,24 +15,120 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  name: z.string().min(1 , {message:'Companion is required'}),
+  subject: z.string().min(1 , {message:'Subject is required'}),
+  topic: z.string().min(1 , {message:'Topic is required'}),
+  voice: z.string().min(1 , {message:'Voice is required'}),
+  style: z.string().min(1 , {message:'Style is required'}),
+  duration: z.coerce.number().min(1 , {message:'Duration is required'}),
 })
 const CompanionForm = () => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-          username: "",
+            name:"",
+            subject:"",
+            topic:"",
+            voice:"",
+            style:"",
+            duration:15,
         },
       })
      
       // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
+      const onSubmit =  async(values: z.infer<typeof formSchema>)=> {
         console.log(values)
       }
   return (
-    <div>CompanionForm</div>
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Companion Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter Your Companion Name" {...field} className="input"/>
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="subject"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Subject</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter Your Companion Name" {...field} className="input"/>
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Companion Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter Your Companion Name" {...field} className="input"/>
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Companion Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter Your Companion Name" {...field} className="input"/>
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Companion Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter Your Companion Name" {...field} className="input"/>
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Companion Name</FormLabel>
+            <FormControl>
+              <Input placeholder="Enter Your Companion Name" {...field} className="input"/>
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
   )
 }
 
